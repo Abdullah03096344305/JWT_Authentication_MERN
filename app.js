ceda6a9523f25b98e3eb7436b7b3f1c47904d007
Abdullah03096344305 <abdullah03096344305@gmail.com>
2026-01-04 17:38:48 +0500

    password hashing complete till now

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const userModel = require("./models/user");
+const bcrypt = require("bcrypt");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/create", (req, res) => {
+  let { username, email, password, age } = req.body;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let createdUser = await userModel.create({
+          username,
+          email,
+          password: hash,
+          age
+      })
+      res.send(createdUser);
+    });
+  });
+});
+
+app.listen(3000);
